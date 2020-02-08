01797f0ac41b4b5874f4b1e95aa8dc8153ce38e0
Swakhil Mattapalli <mattapalliswakhil@gmail.com>
2020-02-08 23:53:49 +0530

    Added time zone

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'portfolio',
         'USER': 'root',
-        'PASSWORD': 'Wipro@123',
+        'PASSWORD': '',
         'PORT': '', 
     }
 }
@@ -110,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Kolkata'
 
 USE_I18N = True
 
